7d5094220417f9a9dfc632808e0069b3941e6047
unknown <sophiecodes@gmail.com>
2020-07-17 10:48:45 +0100

    Created Item Controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        
+    }
+    // public IActionResult Index()
+    //     {
+    //         return View("Index");
+    //     }
+
+}
